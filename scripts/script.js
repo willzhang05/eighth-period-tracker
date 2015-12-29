d1084fa72467673ecd705ad5e2acf9cd59c3d828
William Zhang <willzhang05@gmail.com>
2015-12-29 14:16:25 -0500

    Remove unnecessary code

--- scripts/script.js
+++ scripts/script.js
@@ -26,10 +26,6 @@ window.onresize = function() {
 	scale = 1280 / (1.49 * w);
 	element.style.width =  (scale * 100) + "%";
 }*/
-/*
-<a class="twitter-timeline" data-dnt="true" href="https://twitter.com/rejectedtjTODAY" data-widget-id="667454451305836544">Tweets by @rejectedtjTODAY</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+"://platform.twitter.com/widgets.js";fjs.parentNode.insertBefore(js,fjs);}}(document,"script","twitter-wjs");</script>
-*/
 document.getElementById("login-form").onsubmit = function() {
 	loginUser();
 }
